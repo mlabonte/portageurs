c87869e1d1fb32f8778c32372debdf40bc6f4788
martin <martin@martinlabonte.com>
2014-12-02 21:50:22 -0500

    Premier commit.

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* App Module */
+/* see http://scotch.io/tutorials/javascript/single-page-apps-with-angularjs-routing-and-templating */
+var phonecatApp = angular.module('phonecatApp', ['ngRoute']);
+
+phonecatApp.config(function($routeProvider) {
+    $routeProvider.
+      when('/a', {
+        templateUrl: 'partials/a.html',
+        controller: 'aCtrl'
+      }).
+      when('/b', {
+        templateUrl: 'partials/b.html',
+        controller: 'bCtrl'
+      }).
+      otherwise({
+        redirectTo: '/a'
+      });
+  });
+
+// create the controller and inject Angular's $scope
+phonecatApp.controller('aCtrl', function($scope) {
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+phonecatApp.controller('bCtrl', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
