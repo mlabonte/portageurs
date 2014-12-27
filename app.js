bec450e121979a942086455bf8ed581120f2330a
martin <martin@martinlabonte.com>
2014-12-27 16:01:52 -0500

    Ajout de séparateur pour les mois.

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 'use strict';
 
-/* App Module */
-/* see http://scotch.io/tutorials/javascript/single-page-apps-with-angularjs-routing-and-templating */
-var phonecatApp = angular.module('phonecatApp', ['ngRoute']);
+var calendrierApp = angular.module('calendrierApp', ['ngRoute']);
 
-phonecatApp.config(function($routeProvider) {
+calendrierApp.config(function($routeProvider) {
     $routeProvider.
       when('/', {
         templateUrl: 'partials/liste.html',
-        controller: 'aCtrl'
+        controller: 'listeCtrl'
       }).
       when('/details/:sortieid?', {
         templateUrl: 'partials/details.html',
-        controller: 'bCtrl'
+        controller: 'detailsCtrl'
       }).
       otherwise({
         redirectTo: '/'
@@ -28,23 +26,44 @@ function transformer_sortie(sortie){
     if( desc && desc.indexOf("href")>0 ){
         sortie["description"] = "";
     }
+
+    var mois_idx = sortie["debut"].match(/(\d{4})-(\d{2})-(\d{2})/)[2];
+    sortie["mois_idx"] = mois_idx - 1;
+    
     return sortie;
 }
   
-// create the controller and inject Angular's $scope
-phonecatApp.controller('aCtrl', function($scope, $http, $location) {
+// Controlleur de la liste
+calendrierApp.controller('listeCtrl', function($scope, $http, $location) {
   $http.get('//www.portageurs.qc.ca/cgi-bin/calendrierjson.pl').success(function(data) {
     var sorties = [];
+    $scope.mois = [ {"nom": "Janvier", "sorties": []},
+                    {"nom": "Février",  "sorties": []},
+                    {"nom": "Mars", "sorties": []},
+                    {"nom": "Avril",  "sorties": []},
+                    {"nom": "Mai", "sorties": []},
+                    {"nom": "Juin", "sorties": []},
+                    {"nom": "Juillet", "sorties": []},
+                    {"nom": "Août", "sorties": []},
+                    {"nom": "Septembre", "sorties": []},
+                    {"nom": "Octobre", "sorties": []},
+                    {"nom": "Novembre", "sorties": []},
+                    {"nom": "Décembre", "sorties": []} ];
+                    
     for(var i=0; i<data["sorties"].length; i++){
         var sortie = data["sorties"][i];
         sortie = transformer_sortie(sortie);
-        sorties.push(sortie);
+        $scope.mois[sortie["mois_idx"]]["sorties"].push(sortie);
     }
 
-    $scope.sorties = {"sorties": sorties};
+    $scope.sorties = {"sorties": sorties, "annee": data["anne"]};
     $scope.go = function(id_sortie){
         $location.path( "/details/" + id_sortie );
     };
+    $scope.filtre_type = function(actual, expected){
+        //alert(actual);
+        return true;
+    }
   }).error(function(data, status, headers, config) {
       alert(status);
       $scope.sorties['status'] = status;
@@ -53,7 +72,8 @@ phonecatApp.controller('aCtrl', function($scope, $http, $location) {
   });
 });
 
-phonecatApp.controller('bCtrl', function($scope, $routeParams, $sce) {
+// Controleur de la page de détails (iframe pour l'instant)
+calendrierApp.controller('detailsCtrl', function($scope, $routeParams, $sce) {
     var url = "//www.portageurs.qc.ca/cgi-bin/details.pl?id=" + $routeParams.sortieid;
     $scope.sortieUrl = $sce.trustAsResourceUrl(url);
 });
